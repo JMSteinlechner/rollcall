16723e86201fffe62ef8369318867383e9b94fd3
JMSteinlechner <jakob@steinlechners.at>
2025-01-15 22:07:13 +0100

    deleted target folders

--- tool/src/java/org/sakaiproject/rollcall/tool/MyApplication.java
+++ tool/src/java/org/sakaiproject/rollcall/tool/MyApplication.java
@@ -28,98 +28,98 @@ public class MyApplication extends WebApplication {
 	/**
 	 * Configure your app here
 	 */
-	@Override
-	protected void init() {
+    @Override
+    protected void init() {
+        super.init();
 
         // Disable the CSP header (we can handle this in root of application)
         getCspSettings().blocking().disabled();
 
-		//Configure for Spring injection
+        //Configure for Spring injection
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
 
-		//Don't throw an exception if we are missing a property, just fallback
-		getResourceSettings().setThrowExceptionOnMissingResource(false);
-		
-		//Remove the wicket specific tags from the generated markup
-		getMarkupSettings().setStripWicketTags(true);
+        //Don't throw an exception if we are missing a property, just fallback
+        getResourceSettings().setThrowExceptionOnMissingResource(false);
 
-				
-		// On Wicket session timeout, redirect to main page
-		getApplicationSettings().setPageExpiredErrorPage(FirstPage.class);
-		getApplicationSettings().setAccessDeniedPage(FirstPage.class);
+        //Remove the wicket specific tags from the generated markup
+        getMarkupSettings().setStripWicketTags(true);
+
+        // On Wicket session timeout, redirect to main page
+        getApplicationSettings().setPageExpiredErrorPage(FirstPage.class);
+        getApplicationSettings().setAccessDeniedPage(FirstPage.class);
 
         getRequestCycleListeners().add(new IRequestCycleListener() {
 
             public void onBeginRequest()
             {
-            // optionally do something at the beginning of the request
+                // optionally do something at the beginning of the request
             }
 
             public void onEndRequest()
             {
-            // optionally do something at the end of the request
+                // optionally do something at the end of the request
             }
 
             public IRequestHandler onException(RequestCycle cycle, Exception ex)
             {
-            // optionally do something here when there's an exception
+                // optionally do something here when there's an exception
 
-            // then, return the appropriate IRequestHandler, or "null"
-            // to let another listener handle the exception
+                // then, return the appropriate IRequestHandler, or "null"
+                // to let another listener handle the exception
                 ex.printStackTrace();
                 return null;
             }
 
             @Override
             public void onBeginRequest(RequestCycle arg0) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onDetach(RequestCycle arg0) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onEndRequest(RequestCycle arg0) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onExceptionRequestHandlerResolved(
                     RequestCycle arg0, IRequestHandler arg1, Exception arg2) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onRequestHandlerExecuted(RequestCycle arg0,
                                                  IRequestHandler arg1) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onRequestHandlerResolved(RequestCycle arg0,
                                                  IRequestHandler arg1) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onRequestHandlerScheduled(RequestCycle arg0,
                                                   IRequestHandler arg1) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
 
             @Override
             public void onUrlMapped(RequestCycle arg0,
                                     IRequestHandler arg1, Url arg2) {
-            // TODO Auto-generated method stub
+                // TODO Auto-generated method stub
 
             }
         });
@@ -127,8 +127,8 @@ public class MyApplication extends WebApplication {
 
 
 
-		//to put this app into deployment mode, see web.xml
-	}
+        //to put this app into deployment mode, see web.xml
+    }
 	
 
 	
