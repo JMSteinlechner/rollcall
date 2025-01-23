9da1ad63ae7de5df23183fc7db65d575caee3ea1
Jakob Windisch <wj4469@mci4me.at>
2025-01-23 21:02:00 +0100

    implementation with @Autowired since previous one didn't build

--- tool/src/java/org/sakaiproject/rollcall/tool/AttendanceCallbackController.java
+++ tool/src/java/org/sakaiproject/rollcall/tool/AttendanceCallbackController.java
@@ -1,12 +1,16 @@
 package org.sakaiproject.rollcall.tool;
 
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class AttendanceCallbackController {
 
 
-    public void handleCallback(String meetingID) {
+    @EventListener
+    public void handleCallback(Map<String, Object> meetingInfo) {
         // TODO: implement callback when meeting ends
         System.out.println("AttendanceCallbackController handleCallback");
     }
