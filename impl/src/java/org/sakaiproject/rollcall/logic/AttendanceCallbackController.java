d2fe8ed450e5553c0c3aa5ac701dc710a5e17d6f
Jakob Windisch <wj4469@mci4me.at>
2025-01-28 11:56:52 +0100

    successful dependency injection

--- impl/src/java/org/sakaiproject/rollcall/logic/AttendanceCallbackController.java
+++ impl/src/java/org/sakaiproject/rollcall/logic/AttendanceCallbackController.java
@@ -1,15 +1,8 @@
-package org.sakaiproject.rollcall.tool;
-
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+package org.sakaiproject.rollcall.logic;
 
 import java.util.Map;
 
-@Component
-public class AttendanceCallbackController {
-
-
-    @EventListener
+public class AttendanceCallbackController{
     public void handleCallback(Map<String, Object> meetingInfo) {
         // TODO: implement callback when meeting ends
         System.out.println("AttendanceCallbackController handleCallback");
