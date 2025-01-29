aa820d44ea9de39952ca5f53af5ace5db1d4c6af
JMSteinlechner <jakob@steinlechners.at>
2025-01-29 19:27:48 +0100

    fixing active tabs

--- tool/src/java/org/sakaiproject/rollcall/tool/Rollcall.java
+++ tool/src/java/org/sakaiproject/rollcall/tool/Rollcall.java
@@ -1,11 +1,14 @@
 package org.sakaiproject.rollcall.tool;
 
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.request.IRequestCycle;
 import org.apache.wicket.request.IRequestHandler;
 import org.apache.wicket.request.cycle.IRequestCycleListener;
 import org.apache.wicket.request.cycle.RequestCycle;
 import org.apache.wicket.request.Url;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
+
+import org.sakaiproject.rollcall.framework.RollcallStringResourceLoader;
 import org.sakaiproject.rollcall.tool.pages.FirstPage;
 
 /**
@@ -26,17 +29,24 @@ public class Rollcall extends WebApplication {
     protected void init() {
         super.init();
 
+
         // Disable the CSP header (we can handle this in root of application)
         getCspSettings().blocking().disabled();
 
         //Configure for Spring injection
         getComponentInstantiationListeners().add(new SpringComponentInjector(this));
 
+        // Add ResourceLoader that integrates with Sakai's Resource Loader
+        getResourceSettings().getStringResourceLoaders().add(0, new RollcallStringResourceLoader());
         //Don't throw an exception if we are missing a property, just fallback
         getResourceSettings().setThrowExceptionOnMissingResource(false);
 
         //Remove the wicket specific tags from the generated markup
         getMarkupSettings().setStripWicketTags(true);
+
+        // Don't add any extra tags around a disabled link (default is <em></em>)
+        //getMarkupSettings().setDefaultBeforeDisabledLink(null);
+        //getMarkupSettings().setDefaultAfterDisabledLink(null);
 
         // On Wicket session timeout, redirect to main page
         getApplicationSettings().setPageExpiredErrorPage(FirstPage.class);
@@ -120,7 +130,10 @@ public class Rollcall extends WebApplication {
 
 
 
-
+        // cleanup the HTML
+        getMarkupSettings().setStripWicketTags(true);
+        getMarkupSettings().setStripComments(true);
+        getMarkupSettings().setCompressWhitespace(true);
         //to put this app into deployment mode, see web.xml
     }
 	
