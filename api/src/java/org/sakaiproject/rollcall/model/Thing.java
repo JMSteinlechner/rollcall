a2faf2865d52dd8ee74014fe3d7f544be18b87c0
JMSteinlechner <jakob@steinlechners.at>
2025-02-01 19:13:37 +0100

    trying with hibernate

--- api/src/java/org/sakaiproject/rollcall/model/Thing.java
+++ api/src/java/org/sakaiproject/rollcall/model/Thing.java
@@ -1,22 +1,45 @@
 package org.sakaiproject.rollcall.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-/**
- * An example thing
- * 
- * @author Steve Swinsburg (steve.swinsburg@anu.edu.au)
- *
- */
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name = "PROJECT_THINGS")
 public class Thing implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+
+	@Column(name = "TITLE", nullable = false)
 	private String name;
+
+	// Constructors
+	public Thing() {}
+
+	public Thing(String name) {
+		this.name = name;
+	}
+
+	// Getters and Setters
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Thing{id=" + id + ", name='" + name + "'}";
+	}
 }
