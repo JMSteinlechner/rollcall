a166fd74cdfa3b37ba8c5089dfe4cbd3f5671383
lilli <sl1176@mci4me.at>
2025-02-03 20:08:22 +0100

    changes diagram

--- tool/src/java/org/sakaiproject/rollcall/tool/pages/CourseStatisticPage.java
+++ tool/src/java/org/sakaiproject/rollcall/tool/pages/CourseStatisticPage.java
@@ -31,98 +31,103 @@ import org.sakaiproject.rollcall.tool.model.Attendant;
  */
 public class CourseStatisticPage extends BasePage {
 
-	private static final String DATE_TIME_FORMAT = "dd-MMM-yyyy HH:mm:ss";
-
 	private static final long serialVersionUID = 1L;
-
 
 	public CourseStatisticPage() {
 		disableLink(this.statisticLink);
 		List<Attendant> attendantList = setAttendee();
 		setAttendanceTime(attendantList);
 
+		add(new Image("attendanceTimeChartImage", new DynamicImageResource() {
+			@Override
+			protected byte[] getImageData(IResource.Attributes attributes) {
+				return generateAttendanceTimeChartImage();
+			}
+		}));
 	}
 
 
-private List<Attendant> setAttendee() {
-	List<Attendant> attendantList = Arrays.asList(
-			new Attendant(1L, "Lisa", "Tester"),
-			new Attendant(2L, "Markus", "Muster"),
-			new Attendant(3L, "Jakob", "Maier")
-	);
+	private List<Attendant> setAttendee() {
+		List<Attendant> attendantList = Arrays.asList(
+				new Attendant(1L, "Lisa", "Tester"),
+				new Attendant(2L, "Markus", "Muster"),
+				new Attendant(3L, "Jakob", "Maier")
+		);
 
-	ListView<Attendant> listViewAttendant = new ListView<>("attendantList", attendantList) {
-		@Override
-		protected void populateItem(ListItem<Attendant> item) {
-			item.add(new Label("number", new PropertyModel<>(item.getModel(), "id")));
-			item.add(new Label("firstname", new PropertyModel<>(item.getModel(), "firstname")));
-			item.add(new Label("lastname", new PropertyModel<>(item.getModel(), "lastname")));
-		}
-	};
+		ListView<Attendant> listViewAttendant = new ListView<>("attendantList", attendantList) {
+			@Override
+			protected void populateItem(ListItem<Attendant> item) {
+				item.add(new Label("number", new PropertyModel<>(item.getModel(), "id")));
+				item.add(new Label("firstname", new PropertyModel<>(item.getModel(), "firstname")));
+				item.add(new Label("lastname", new PropertyModel<>(item.getModel(), "lastname")));
+			}
+		};
 
-	add(listViewAttendant);
+		add(listViewAttendant);
 
-	return attendantList;
-}
+		return attendantList;
+	}
 
-private void setAttendanceTime(List<Attendant> attendantList) {
-	List<AttendanceTime> attendanceTimeList = Arrays.asList(
-			new AttendanceTime(1L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
-			new AttendanceTime(2L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
-			new AttendanceTime(3L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
-			new AttendanceTime(4L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(20))),
-			new AttendanceTime(5L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(25))),
-			new AttendanceTime(6L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(30))),
-			new AttendanceTime(7L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(35))),
-			new AttendanceTime(8L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
-			new AttendanceTime(9L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
-			new AttendanceTime(10L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
-			new AttendanceTime(11L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(20))),
-			new AttendanceTime(12l, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(25))),
-			new AttendanceTime(15L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
-			new AttendanceTime(16L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
-			new AttendanceTime(17L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
-			new AttendanceTime(20L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(30))),
-			new AttendanceTime(21L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(35)))
-	);
+	private void setAttendanceTime(List<Attendant> attendantList) {
+		List<AttendanceTime> attendanceTimeList = Arrays.asList(
+				new AttendanceTime(1L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
+				new AttendanceTime(2L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
+				new AttendanceTime(3L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
+				new AttendanceTime(4L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(20))),
+				new AttendanceTime(5L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(25))),
+				new AttendanceTime(6L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(30))),
+				new AttendanceTime(7L, 1L, 1L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(35))),
+				new AttendanceTime(8L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
+				new AttendanceTime(9L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
+				new AttendanceTime(10L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
+				new AttendanceTime(11L, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(20))),
+				new AttendanceTime(12l, 1L, 2L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(25))),
+				new AttendanceTime(15L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(5))),
+				new AttendanceTime(16L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(10))),
+				new AttendanceTime(17L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(15))),
+				new AttendanceTime(20L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(30))),
+				new AttendanceTime(21L, 1L, 3L, LocalDateTime.of(LocalDate.now(), LocalTime.now().minusMinutes(35)))
+		);
 
-	ListView<AttendanceTime> listViewAttendantTime = new ListView<>("attendanceTimeList", attendanceTimeList) {
-		@Override
-		protected void populateItem(ListItem<AttendanceTime> item) {
-			item.add(new Label("studentId", new PropertyModel<>(item.getModel(), "studentId")));
-			item.add(new Label("attendanceTime", new PropertyModel<>(item.getModel(), "attendanceTime")));
-		}
-	};
+		ListView<AttendanceTime> listViewAttendantTime = new ListView<>("attendanceTimeList", attendanceTimeList) {
+			@Override
+			protected void populateItem(ListItem<AttendanceTime> item) {
+				item.add(new Label("studentId", new PropertyModel<>(item.getModel(), "studentId")));
+				item.add(new Label("attendanceTime", new PropertyModel<>(item.getModel(), "attendanceTime")));
+			}
+		};
 
-	add(listViewAttendantTime);
+		add(listViewAttendantTime);
 
-	DefaultCategoryDataset dataset = new DefaultCategoryDataset();
-	for(Attendant attendant : attendantList){
-		for(AttendanceTime time : attendanceTimeList){
-			if(time.getStudentId() == attendant.getId()) {
-				dataset.addValue(1, attendant.getFirstname(), time.getAttendanceTime());
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		for (Attendant attendant : attendantList) {
+			for (AttendanceTime time : attendanceTimeList) {
+				if (time.getStudentId() == attendant.getId()) {
+					dataset.addValue(1, attendant.getFirstname(), time.getAttendanceTime());
+				}
 			}
 		}
+
+		// Create the bar chart
+		JFreeChart attendanceTimeLineChart = ChartFactory.createLineChart(
+				"Anwesende per Zeiteinheit",         // Chart title
+				"Zeit",                 // X-axis label
+				"Anzahl Anwesende",                    // Y-axis label
+				dataset                    // Dataset
+		);
+
+		// Convert chart to image
+		BufferedImage image = chart.createBufferedImage(600, 400);
+		return toByteArray(image);
 	}
 
-	// Create the bar chart
-	JFreeChart barChart = ChartFactory.createBarChart(
-			"Anwesende per Zeiteinheit",         // Chart title
-			"Zeit",                 // X-axis label
-			"Anzahl Anwesende",                    // Y-axis label
-			dataset                    // Dataset
-	);
-
-	// Create a file to save the chart image
-	File barChartFile = new File(getPath()+"/charts/barChart.png");
-	try {
-		ChartUtils.saveChartAsPNG(barChartFile, barChart, 600, 400);
-	} catch (IOException e) {
-		e.printStackTrace();
+	private byte[] toByteArray(BufferedImage image) {
+		try (java.io.ByteArrayOutputStream baos = new java.io.ByteArrayOutputStream()) {
+			javax.imageio.ImageIO.write(image, "png", baos);
+			return baos.toByteArray();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-
-	// Display the bar chart image in Wicket
-	Image attendanceTimeChart = new Image("attendanceTimeChart", new Model<>(barChartFile.getPath()));
-	add(attendanceTimeChart);
-}
 }
